1ddc323eb837facf67f10a6bf0502d34a1e3f928
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2023-03-14 12:21:09 -0400

    Add minor typing

--- dcm2mha_cnvtr/__main__.py
+++ dcm2mha_cnvtr/__main__.py
@@ -1,8 +1,8 @@
 from dcm2mha_cnvtr.dcm2mha_cnvtr import Dcm2mha_cnvtr
 
 
-def main():
-    chris_app = Dcm2mha_cnvtr()
+def main() -> None:
+    chris_app: Dcm2mha_cnvtr = Dcm2mha_cnvtr()
     chris_app.launch()
 
 
