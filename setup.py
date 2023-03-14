1cc6a38072641d8265237c28f7b448079f7a879d
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2023-03-14 18:14:07 -0400

    Bump to trigger rebuild with latest versions of pftag and pflog

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(path.join(path.dirname(path.abspath(__file__)), 'README.rst')) as f:
 
 setup(
     name             = 'dcm2mha_cnvtr',
-    version          = '1.1.12',
+    version          = '1.1.14',
     description      = 'An app to convert dcm files to mha and vice-versa',
     long_description = readme,
     author           = 'FNNDSC',
